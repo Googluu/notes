47266f86349ac3a4df07f5153ba8856735102c0d
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-08-19 08:30:04 -0500

    Conectado a la base de datos mongo db atlas :leaves:

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const app = require('./server');
+const db = require('./database');
+
+const {config} = require('./config/config');
+
+// connect db
+db(config.dbUrl)
 
 app.listen(app.get('port'), () => {
     console.log('Server running at port', app.get('port'));
